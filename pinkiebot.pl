adf6098c2ce465bd8db2bc17568094f87b128f88
WaveHack <email@wavehack.net>
2012-01-17 09:54:50 +0100

    Changed version to 1.1.2.

--- pinkiebot.pl
+++ pinkiebot.pl
@@ -26,7 +26,7 @@ use URI::Title 'title';
 use warnings;
 use strict;
 
-my $version = '1.1.1';
+my $version = '1.1.2';
 my $botinfo = ('PinkieBot v' . $version . ' by WaveHack. See https://bitbucket.org/WaveHack/pinkiebot/ for more info, command usage and source code.');
 
 # --- Initialization ---
