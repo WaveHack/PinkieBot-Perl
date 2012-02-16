2441db265473ef95f132b4a240ba65b167c9238e
WaveHack <email@wavehack.net>
2012-02-16 16:53:22 +0100

    Minor wording change.

--- modules/log.pm
+++ modules/log.pm
@@ -1,7 +1,7 @@
 package PinkieBot::Module::Log;
 use base 'PinkieBot::Module';
 
-my %dbhsth;
+my %dbsth;
 
 sub init {
 	my ($self, $bot, $message, $args) = @_;
@@ -10,7 +10,7 @@ sub init {
 	$self->createTableIfNotExists('activity', $message);
 
 	# Prepared statements
-	$dbhsth{activity} = $self->{bot}->{db}->prepare('INSERT INTO activity (type, timestamp, who, raw_nick, channel, body, address) VALUES (?, UNIX_TIMESTAMP(), ?, ?, ?, ?, ?);');
+	$dbsth{activity} = $self->{bot}->{db}->prepare('INSERT INTO activity (type, timestamp, who, raw_nick, channel, body, address) VALUES (?, UNIX_TIMESTAMP(), ?, ?, ?, ?, ?);');
 
 	# Register hooks
 	$self->registerHook('said', \&handleSaid);
@@ -27,52 +27,52 @@ sub init {
 
 sub handleSaid {
 	my ($bot, $message) = @_;
-	$dbhsth{activity}->execute('said', $message->{who}, $message->{raw_nick}, $message->{channel}, $message->{body}, $message->{address});
+	$dbsth{activity}->execute('said', $message->{who}, $message->{raw_nick}, $message->{channel}, $message->{body}, $message->{address});
 }
 
 sub handleEmoted {
 	my ($bot, $message) = @_;
-	$dbhsth{activity}->execute('emote', $message->{who}, $message->{raw_nick}, $message->{channel}, $message->{body}, $message->{address});
+	$dbsth{activity}->execute('emote', $message->{who}, $message->{raw_nick}, $message->{channel}, $message->{body}, $message->{address});
 }
 
 sub handleNoticed {
 	my ($bot, $message) = @_;
-	$dbhsth{activity}->execute('notice', $message->{who}, $message->{raw_nick}, $message->{channel}, $message->{body}, $message->{address});
+	$dbsth{activity}->execute('notice', $message->{who}, $message->{raw_nick}, $message->{channel}, $message->{body}, $message->{address});
 }
 
 sub handleChanJoin {
 	my ($bot, $message) = @_;
-	$dbhsth{activity}->execute('chanjoin', $message->{who}, $message->{raw_nick}, $message->{channel}, $message->{body}, $message->{address});
+	$dbsth{activity}->execute('chanjoin', $message->{who}, $message->{raw_nick}, $message->{channel}, $message->{body}, $message->{address});
 }
 
 sub handleChanPart {
 	my ($bot, $message) = @_;
-	$dbhsth{activity}->execute('chanpart', $message->{who}, $message->{raw_nick}, $message->{channel}, $message->{body}, $message->{address});
+	$dbsth{activity}->execute('chanpart', $message->{who}, $message->{raw_nick}, $message->{channel}, $message->{body}, $message->{address});
 }
 
 sub handleTopic {
 	my ($bot, $message) = @_;
-	$dbhsth{activity}->execute('topic', $message->{who}, $message->{raw_nick}, $message->{channel}, $message->{body}, $message->{address});
+	$dbsth{activity}->execute('topic', $message->{who}, $message->{raw_nick}, $message->{channel}, $message->{body}, $message->{address});
 }
 
 sub handleNickChange {
 	my ($bot, $oldnick, $newnick) = @_;
-	$dbhsth{activity}->execute('nickchange', $oldnick, undef, undef, $newnick, undef);
+	$dbsth{activity}->execute('nickchange', $oldnick, undef, undef, $newnick, undef);
 }
 
 sub handleKicked {
 	my ($bot, $message) = @_;
-	$dbhsth{activity}->execute('kicked', $message->{who}, $message->{kicked}, $message->{channel}, $message->{reason}, undef);
+	$dbsth{activity}->execute('kicked', $message->{who}, $message->{kicked}, $message->{channel}, $message->{reason}, undef);
 }
 
 sub handleUserQuit {
 	my ($bot, $message) = @_;
-	$dbhsth{activity}->execute('userquit', $message->{who}, undef, undef, $message->{body}, undef);
+	$dbsth{activity}->execute('userquit', $message->{who}, undef, undef, $message->{body}, undef);
 }
 
 sub handleMode {
 	my ($bot, $message) = @_;
-	$dbhsth{activity}->execute('mode', undef, $message->{source}, $message->{channel}, ($message->{mode} . ' ' . $message->{args}), undef);
+	$dbsth{activity}->execute('mode', undef, $message->{source}, $message->{channel}, ($message->{mode} . ' ' . $message->{args}), undef);
 }
 
 1;
