c50bc1c8cc65ba803d24c6060d3c90549d27fd9d
WaveHack <email@wavehack.net>
2012-02-16 11:10:34 +0100

    Added some code documentation.

--- pinkiebot.pl
+++ pinkiebot.pl
@@ -74,11 +74,16 @@ $bot->{modules}    = {};
 
 $bot->{cfg} = $cfg;
 
+# PinkieBot uses a hardcoded MySQL link. She used to use SQLite, but I changed
+# it a few revisions ago. Maybe I'll move the database link and the config stuff
+# over to its own module later.
+
 print "Creating database link\n";
 
 $bot->{db} = DBI->connect(sprintf('DBI:mysql:%s;host=%s', $bot->{cfg}->val('mysql', 'database'), $bot->{cfg}->val('mysql', 'host')), $bot->{cfg}->val('mysql', 'username'), $bot->{cfg}->val('mysql', 'password'), {'mysql_enable_utf8' => 1}) or die($DBI::errstr . "\n");
 $bot->{db}->do('SET NAMES utf8');
 
+# Autoload modules if any (set in config)
 if ($cfg->val('irc', 'autoload') ne '') {
 	foreach (split(' ', $cfg->val('irc', 'autoload'))) {
 		print "Auto-loading module '$_'...";
@@ -129,6 +134,8 @@ sub help        { return $botinfo; }
 
 # --- Bot Functions ---
 
+# Returns an array with all available modules. A file ending in .pm in /modules/
+# classifies as an available module.
 sub getAvailableModules {
 	my $self = shift;
 
@@ -140,11 +147,13 @@ sub getAvailableModules {
 	return @availableModules;
 }
 
+# Returns an array of currently loaded modules (both active and inactive ones)
 sub getLoadedModules {
 	my $self = shift;
 	return @{$self->{moduleList}};
 }
 
+# Returns an array of active modules
 sub getActiveModules {
 	my $self = shift;
 
@@ -158,6 +167,9 @@ sub getActiveModules {
 	return @activeModules;
 }
 
+# Loads a module. The require() part is done in an eval{} block to catch parse
+# errors (Perl ftw!) so the mane thread doesn't break when reloading broken
+# module files.
 sub loadModule {
 	my ($self, $module) = @_;
 	my $moduleKey = lc($module);
@@ -174,7 +186,8 @@ sub loadModule {
 
 	my $modulePackage = ("PinkieBot::Module::" . ucfirst($module));
 
-	# Remove package from %INC if it exists so we don't get the cached version
+	# Remove package from %INC if it exists so we don't get the cached version.
+	# This forces require() to actually re-parse the file again.
 	delete $INC{'./modules/' . $moduleKey . '.pm'};
 
 	# Include file and set up object in an eval{} block so we can catch parse
@@ -188,15 +201,18 @@ sub loadModule {
 		push(@{$self->{moduleList}}, $moduleKey);
 	};
 
-	# Return error if eval{}; above failed
+	# Return error if the eval{} block above failed (due parse error most
+	# likely)
 	if ($@) {
 		return { status => 0, code => 2, string => $@ };
 	}
 
-	# Else return success
+	# No perse errors, return success. Huzzah!
 	return { status => 1, code => -1, string => "Module '$module' loaded" };
 }
 
+# Unloads a module (if loaded) and reloads it. Note that non-stored variables
+# are not kept and the whole init()itialization function is called again.
 sub reloadModule {
 	my ($self, $module) = @_;
 
@@ -206,6 +222,7 @@ sub reloadModule {
 	return { status => 1, code => -1, string => "Module '$module' reloaded" };
 }
 
+# Unloads a module from memory.
 sub unloadModule {
 	my ($self, $module) = @_;
 	my $moduleKey = lc($module);
@@ -220,6 +237,7 @@ sub unloadModule {
 	return { status => 1, code => -1, string => "Module '$module' unloaded" };
 }
 
+# Enables or activates a module
 sub enableModule {
 	my ($self, $module) = @_;
 	my $moduleKey = lc($module);
@@ -237,6 +255,10 @@ sub enableModule {
 	return { status => 1, code => -1, string => "Module '$module' enabled" };
 }
 
+# Disables a module. Note that it stays active in memory, including any
+# variables that might have changed. Disabled modules can be reenabled again,
+# using the same state (including variables) prior to disabling if other modules
+# didn't mess with it.
 sub disableModule {
 	my ($self, $module) = @_;
 	my $moduleKey = lc($module);
@@ -254,6 +276,12 @@ sub disableModule {
 	return { status => 1, code => -1, string => "Module '$module' disabled" };
 }
 
+# Registers code to be executed on certain IRC events of type $type. Valid types
+# are: connected, said, emoted, noticed, chanjoin, chanpart, topic, nick_change,
+# kicked, userquit and invited. See CPAN pages for Bot::BasicBot for parameter
+# details and general usage. Event invited isn't in Bot::BasicBot and is built
+# in here. Code hooks are usually registered through module's init() function,
+# but can theoretically be called on the fly from wherever inside a module.
 sub registerHook {
 	my ($self, $module, $type, $function) = @_;
 
@@ -262,6 +290,8 @@ sub registerHook {
 	return { status => 1, code => -1, string => "Hook with type '$type' for module '$module' registered" };
 }
 
+# Unregisters a code hook from a module. $function must contain the exact same
+# code used to register to unregister it.
 sub unregisterHook {
 	my ($self, $module, $type, $function) = @_;
 
@@ -274,6 +304,7 @@ sub unregisterHook {
 	return { status => 1, code => -1, string => "Hook with type '$type' for module '$module' unregistered" };
 }
 
+# Unregisters all hooks from a module
 sub unregisterHooks {
 	my ($self, $module, $type) = @_;
 
@@ -282,27 +313,35 @@ sub unregisterHooks {
 	return { status => 1, code => -1, string => "All hooks with type '$type' for module '$module' unregistered" };
 }
 
+# Returns an array of functions with registered hooks for a module
 sub getHooks {
 	my ($self, $module, $type) = @_;
 
 	return @{$self->{modules}->{$module}->{hooks}->{$type}};
 }
 
+# todo
 sub hookRegistered {
 	my ($self, $module, $type) = @_;
 
 	return @{$self->{modules}->{$module}->{hooks}->{$type}};
 }
 
+# Process hooks of type $type. Loops through every active (!) module and calls
+# hooked function(s), if any. Function calls are done in an eval{} block to
+# catch logic errors and unloads the module if it happens to find an error.
 sub processHooks {
 	my ($self, $type, $data) = @_;
 
+	# For each module
 	while (my ($module, $moduleHash) = each(%{$self->{modules}})) {
 		next if ($moduleHash->{enabled} == 0);
 
+		# For each hook of said type
 		foreach (@{$moduleHash->{hooks}->{$type}}) {
 			eval { $self->$_($data); };
 
+			# Complain and unload module on error
 			if ($@) {
 				$self->say(
 					who     => $data->{who},
@@ -317,16 +356,19 @@ sub processHooks {
 	}
 }
 
+# Returns whether module $module is loaded
 sub moduleLoaded {
 	my ($self, $module) = @_;
 	return (exists($self->{modules}->{lc($module)}) ? 1 : 0);
 }
 
+# Returns whether module $module is both loaded and active
 sub moduleActive {
 	my ($self, $module) = @_;
 	return (($self->moduleLoaded($module) && ($self->{modules}->{lc($module)}->{enabled} == 1)) ? 1 : 0);
 }
 
+# Returns a module
 sub module {
 	my ($self, $module) = @_;
 
@@ -369,11 +411,16 @@ sub leave_channel {
 
 # --- PinkieBot::Module ---
 
+# PinkieBot::Module should be the base of every module. Contains a reference to
+# the bot and contains a handy function for registering hooks.
+
 package PinkieBot::Module;
 
 use warnings;
 use strict;
 
+# Reloading the same module file usually gives redefinition warnings. So disable
+# them here.
 no warnings 'redefine';
 
 sub new {
