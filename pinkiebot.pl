49f4f7ca82d16d68369d61a46b9e651e1627109d
WaveHack <email@wavehack.net>
2012-02-17 13:08:40 +0100

    Updated copyright notice in pinkiebot.pl and added a copyright notice in module files.

--- pinkiebot.pl
+++ pinkiebot.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/perl
 
-# This program is free software: you can redistribute it and/or modify
+#
+# This file is part of PinkieBot.
+#
+# PinkieBot is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
@@ -12,6 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 package PinkieBot;
 use base 'Bot::BasicBot';
