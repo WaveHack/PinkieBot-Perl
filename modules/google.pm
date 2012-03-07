912af2db5a4158ae6761ae4d339995771e631a75
WaveHack <email@wavehack.net>
2012-03-07 23:03:19 +0100

    Google module now reports if there's no results on both web search and image search.

--- modules/google.pm
+++ modules/google.pm
@@ -19,6 +19,17 @@ sub handleSaidWebSearch {
 	my $search = Google::Search->Web(query => $1);
 	my $result = $search->first;
 
+	unless (defined($result)) {
+		$bot->say(
+			who     => $message->{who},
+			channel => $message->{channel},
+			body    => ($message->{who} . ': Sorry, I can\'t find anything for \'' . $searchTerm . '\' on Google.'),
+			address => $message->{address}
+		);
+
+		return;
+	}
+
 	my $title = $result->title;
 	$title =~ s/<b>(.+?)<\/b>/\x02$1\x0F/g;
 
@@ -37,6 +48,17 @@ sub handleSaidImageSearch {
 
 	my $search = Google::Search->Image(query => $1);
 	my $result = $search->first;
+
+	unless (defined($result)) {
+		$bot->say(
+			who     => $message->{who},
+			channel => $message->{channel},
+			body    => ($message->{who} . ': Sorry, I can\'t find anything for \'' . $searchTerm . '\' on Google Images.'),
+			address => $message->{address}
+		);
+
+		return;
+	}
 
 	$bot->say(
 		who     => $message->{who},
