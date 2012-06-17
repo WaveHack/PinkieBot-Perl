1e18e6bc19eccb11c17338048cd48abc320d08db
WaveHack@WaveHack-PC.dynamic.ziggo.nl <WaveHack@WaveHack-PC.dynamic.ziggo.nl>
2012-06-17 16:13:41 +0200

    Added >mlfw command in MLFW module for random MLFW.

--- modules/mlfw.pm
+++ modules/mlfw.pm
@@ -11,61 +11,110 @@ use WWW::Shorten::Bitly;
 my $ua;
 
 sub init {
-        my ($self, $bot, $message, $args) = @_;
+	my ($self, $bot, $message, $args) = @_;
 
-		$ua = LWP::UserAgent->new;
+	$ua = LWP::UserAgent->new;
 
-        # Register hooks
-        $self->registerHook('said', \&handleSaid);
+	# Register hooks
+	$self->registerHook('said', \&handleSaidMLFW);
+	$self->registerHook('said', \&handleSaidMLFWRandom);
 }
 
-sub handleSaid {
-        my ($bot, $message) = @_;
+sub handleSaidMLFW {
+	my ($bot, $message) = @_;
 
-        return unless ($message->{body} =~ /^!mlfw (.+)/);
+	return unless ($message->{body} =~ /^!mlfw (.+)/);
 
-        my $tag = $1;
+	my $response = $ua->get('http://mylittlefacewhen.com/api/search/?limit=1&order=random&tags=["' . join('","', split(',', $1)) . '"]');
 
-		my $response = $ua->get('http://mylittlefacewhen.com/api/search/?limit=1&order=random&tags=["' . $tag . '"]');
+	unless ($response->is_success && ($response->code == 200)) {
+		$bot->say(
+				who     => $message->{who},
+				channel => $message->{channel},
+				body    => ($message->{who} . ': Error while fetching result.'),
+				address => $message->{address}
+		);
 
-		unless ($response->is_success && ($response->code == 200)) {
-			$bot->say(
-					who     => $message->{who},
-					channel => $message->{channel},
-					body    => ($message->{who} . ': Error while fetching result.'),
-					address => $message->{address}
-			);
+		return;
+	}
 
-			return;
-		}
+	my $image = decode_json($response->decoded_content)->[0]{image};
 
-		my $image = decode_json($response->decoded_content)->[0]{image};
+	unless (defined($image) && $image ne '') {
+		$bot->say(
+				who     => $message->{who},
+				channel => $message->{channel},
+				body    => ($message->{who} . ': Nothing found for \'' . $1 . '\' on MLFW.'),
+				address => $message->{address}
+		);
 
-		unless (defined($image) && $image ne '') {
-			$bot->say(
-					who     => $message->{who},
-					channel => $message->{channel},
-					body    => ($message->{who} . ': Nothing found for \'' . $tag . '\' on MLFW.'),
-					address => $message->{address}
-			);
+		return;
+	}
 
-			return;
-		}
+	# Use Bit.ly if we set username/apikey in config
+	if (
+		defined($bot->{cfg}->val('bitly', 'username')) && ($bot->{cfg}->val('bitly', 'username') ne '') &&
+		defined($bot->{cfg}->val('bitly', 'apikey'))   && ($bot->{cfg}->val('bitly', 'apikey') ne '')
+	) {
+		$image = makeashorterlink($image, $bot->{cfg}->val('bitly', 'username'), $bot->{cfg}->val('bitly', 'apikey'));
+	}
 
-		# Use Bit.ly if we set username/apikey in config
-		if (
-			defined($bot->{cfg}->val('bitly', 'username')) && ($bot->{cfg}->val('bitly', 'username') ne '') &&
-			defined($bot->{cfg}->val('bitly', 'apikey'))   && ($bot->{cfg}->val('bitly', 'apikey') ne '')
-		) {
-			$image = makeashorterlink($image, $bot->{cfg}->val('bitly', 'username'), $bot->{cfg}->val('bitly', 'apikey'));
-		}
+	$bot->say(
+			who     => $message->{who},
+			channel => $message->{channel},
+			body    => ($message->{who} . ': MLFW for \'' . $1 . '\': ' . $image),
+			address => $message->{address}
+	);
+}
 
-        $bot->say(
-                who     => $message->{who},
-                channel => $message->{channel},
-                body    => ($message->{who} . ': MLFW for \'' . $tag . '\': ' . $image),
-                address => $message->{address}
-        );
+
+sub handleSaidMLFWRandom {
+	my ($bot, $message) = @_;
+
+	return unless ($message->{body} =~ /^>mlfw (.+)/);
+
+	my $phrase = $1;
+
+	my $response = $ua->get('http://mylittlefacewhen.com/api/faces/?order=random');
+
+	unless ($response->is_success && ($response->code == 200)) {
+		$bot->say(
+			who     => $message->{who},
+			channel => $message->{channel},
+			body    => ($message->{who} . ': Error while fetching result.'),
+			address => $message->{address}
+		);
+
+		return;
+	}
+
+	my $image = decode_json($response->decoded_content)->[0]{image};
+
+	unless (defined($image) && $image ne '') {
+		$bot->say(
+				who     => $message->{who},
+				channel => $message->{channel},
+				body    => ($message->{who} . ': Error while fetching result.'),
+				address => $message->{address}
+		);
+
+		return;
+	}
+
+	# Use Bit.ly if we set username/apikey in config
+	if (
+		defined($bot->{cfg}->val('bitly', 'username')) && ($bot->{cfg}->val('bitly', 'username') ne '') &&
+		defined($bot->{cfg}->val('bitly', 'apikey'))   && ($bot->{cfg}->val('bitly', 'apikey') ne '')
+	) {
+		$image = makeashorterlink($image, $bot->{cfg}->val('bitly', 'username'), $bot->{cfg}->val('bitly', 'apikey'));
+	}
+
+	$bot->say(
+			who     => $message->{who},
+			channel => $message->{channel},
+			body    => ($message->{who} . ': MFW for \'' . $phrase . '\': ' . $image),
+			address => $message->{address}
+	);
 }
 
 1;
