2cd83c923e8ede91d5d211cd643509d4f2a2eb59
WaveHack <email@wavehack.net>
2012-06-29 23:06:23 +0200

    Added option for reloading multiple modules in the Admin module.

--- modules/admin.pm
+++ modules/admin.pm
@@ -145,9 +145,23 @@ sub handleSaidReloadModule {
 	return unless ($bot->addressed($message) && ($message->{body} =~ /^reload(?: module)? ([^ ]+)(?: (.+))?/));
 	return if ($bot->moduleActive('auth') && !$bot->module('auth')->checkAuthorization($bot, $message, 7));
 
-	my $ret = $bot->reloadModule($1, $message, $2);
+	my $module = $1;
+	my $args   = $2;
 
-	$bot->reply("$ret->{string} [Status: $ret->{status}, Code: $ret->{code}]", $message);
+	# Check if we're reloading multiple modules. Arguments are not supported here
+	if ($module =~ /,/) {
+		my @modules = split(',', $module);
+
+		foreach (@modules) {
+			my $ret = $bot->reloadModule($_, $message);
+			$bot->reply("$ret->{string} [Status: $ret->{status}, Code: $ret->{code}]", $message);
+		}
+
+	# Single module, arguments supported
+	} else {
+		my $ret = $bot->reloadModule($module, $message, $args);
+		$bot->reply("$ret->{string} [Status: $ret->{status}, Code: $ret->{code}]", $message);
+	}
 }
 
 sub handleSaidEnableModule {
