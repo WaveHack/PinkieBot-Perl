48b7445caef2ff5bee97624753ab885f1415b187
rctgamer3 <rctgamer3@gmail.com>
2019-02-16 15:43:37 +0100

    Fixed broken DDG module

--- modules/ddg.pm
+++ modules/ddg.pm
@@ -6,12 +6,12 @@ use strict;
 
 use WWW::DuckDuckGo;
 
-my $ddg;
+my $duck;
 
 sub init {
 	my ($self, $bot, $message, $args) = @_;
 
-	$ddg = WWW::DuckDuckGo->new;
+	$duck = WWW::DuckDuckGo->new(safeoff => '1');
 
 	# Register hooks
 	$self->registerHook('said', \&handleSaid);
@@ -23,11 +23,20 @@ sub handleSaid {
 	return unless ($message->{body} =~ /^!ddg (.+)/);
 
 	my $searchTerm = $1;
-	my $zci = $ddg->zci($1);
+	my $zci = $duck->zeroclickinfo($1);
+	# $bot->reply(('DDG query: ' . $searchTerm), $message);
+
+	$bot->say({%$message, body => "Redirected to " . $zci->redirect}) if $zci->has_redirect;
 
 	if ($zci->has_answer) {
 		$bot->reply(('DDG Answer: ' . $zci->heading . ': ' . $zci->answer), $message);
 		return;
+	}
+
+	if ($zci->has_heading) {
+		my $heading = $zci->heading;
+		$heading .= " (" . $zci->type_long . ")" if $zci->has_type;
+		$bot->say({%$message, body => $heading});
 	}
 
 	if ($zci->has_definition) {
@@ -39,7 +48,38 @@ sub handleSaid {
 		$bot->reply(('DDG Abstract Text: ' . $zci->heading . ': ' . $zci->abstract_text), $message);
 		return;
 	}
+
+	if ($zci->type_long eq 'disambiguation') {
+
+		if ($zci->has_default_related_topics) {
+			$bot->say({%$message, body => "Related topics:"});
+
+			for (@{$zci->default_related_topics}) {
+				if ($_->has_text or $_->has_first_url) {
+					my $topic = ' — ';
+					$topic .= $_->text if $_->has_text;
+					$topic .= ' [' if $_->has_text and $_->has_first_url;
+					$topic .= $_->first_url->as_string if $_->has_first_url;
+					$topic .= ']' if $_->has_text and $_->has_first_url;
+					$bot->say({%$message, body => $topic});
+				}
+			}
+		}
+
+		if ($zci->has_results) {
+			$bot->say({%$message, body => "Other results:"});
+			for (@{$zci->results}) {
+				if ($_->has_text or $_->has_first_url) {
+					my $result = ' — ';
+					$result .= $_->text if $_->has_text;
+					$result .= ' [' if $_->has_text and $_->has_first_url;
+					$result .= $_->first_url->as_string if $_->has_first_url;
+					$result .= ']' if $_->has_text and $_->has_first_url;
+					$bot->say({%$message, body => $result});
+				}
+			}
+		}
+	}
 }
 
 1;
-
